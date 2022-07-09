89980de51cf40d1f6b6d6d1f8581c748feeb4b14
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-07-09 23:30:11 +0700

    fixed: cluster name

--- main.tf
+++ main.tf
@@ -37,9 +37,9 @@ provider "kubectl" {
 }
 provider "helm" {
   kubernetes {
-    host                   = azurerm_kubernetes_cluster.senior-project-test-aks.kube_config.0.host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.senior-project-test-aks.kube_config.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.senior-project-test-aks.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.senior-project-test-aks.kube_config.0.cluster_ca_certificate)
+    host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
   }
 }
